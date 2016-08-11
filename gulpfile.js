92dd641258b12fb31733bd2f3af5149c234faa06
rian pauzi <rianpauzi@rians-MacBook-Pro.local>
2016-08-11 22:30:54 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css/'))
+});
+
+gulp.task('default', function() {
+  gulp.watch('sass/**/*.scss', ['styles']);
+});
